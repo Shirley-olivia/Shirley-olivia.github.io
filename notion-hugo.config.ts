8444a3e62ff4d151635d5c150317b5978f6776bb
Shirley <90751427+Shirley-olivia@users.noreply.github.com>
2023-11-30 09:55:20 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/Notion-DoIt-1990cb0ab0e045d0a8ff28fbcd9f454f?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
